63b9c040b63077bc8d1a4c1f5862bebb639a076b
rodrigowebdev <rodrigo.queiroz.chagas@gmail.com>
2020-01-26 13:07:05 -0300

    MVP

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,94 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { Component } from "react"
+import axios from "axios"
+import api from "../services/api"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import logo from "../images/Starwars.png"
+import user from "../images/user-icon.png"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+//https://swapi.co/api/people/
+
+class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      info: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get("https://swapi.co/api/people/")
+      .then(data => {
+        let _data = data.data.results;
+
+        console.log(" DATA > ", _data);
+
+        this.setState({
+          info: _data
+        })
+
+        /*this.state.info.map( e => {
+          ili
+        })
+
+        this.state.info.map( (e , i) => {
+
+          let starships = e.starships;
+          //console.log("STARSHIPS > ", starships);
+          if(starships.length > 0){
+            starships.map( j => {
+              axios.get(j)
+                .then( k => {
+                  //console.log("StarShip GET >>>", k.data.name);
+                  //console.log(this.state.info);
+                })
+            })
+          }
+        })*/
+
+        console.log(">>>>", this.state.info);
+      })
+      .catch(error => {
+        // handle error
+        console.log(error);
+      })
+      .then(() => {
+      });
+  }
+
+  render() {
+
+    return (
+      <>
+        <div className="container">
+          <img className="logo" src={logo} />
+          <h2 className="subtitle">Characters</h2>
+          <ul>
+            {
+              this.state.info.map(i => (
+                <li key={i.name}>
+                  <div className="char">
+                    <img src={user} />
+                    <div>
+                      <h1 className="char-name">{i.name}</h1>
+                      <span className="char-birth">{i.birth_year}</span>
+                      <span className="char-gender">{i.birth_year}</span>
+                      <span className="char-birth">{i.gender}</span>
+                      <span className="char-species"></span>
+                    </div>
+                  </div>
+                </li>
+              ))
+            }
+
+          </ul>
+        </div>
+
+        <footer>
+          Developed by <a href="http://rqueiroz.netlify.com/" target="_blank"> me </a> :)
+        </footer>
+      </>
+    )
+  }
+}
 
 export default IndexPage

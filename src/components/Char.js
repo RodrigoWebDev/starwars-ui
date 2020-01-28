03e211190f6cbf3c21a828ba890bf7d373e0371a
rodrigowebdev <rodrigo.queiroz.chagas@gmail.com>
2020-01-28 14:08:16 -0300

    Starships

--- src/components/Char.js
+++ src/components/Char.js
@@ -1,25 +1,77 @@
-import React from "react"
+import React, { Component } from "react"
 import user from "../images/user-icon.png"
 import "../styles/char.css"
+import axios from "axios"
+import Swal from 'sweetalert2'
 
-const Char = ({ info }) => (
-    <div className="char">
-        <img src={user} />
-        <div>
-            <h2 className="char-name">{info.name}</h2>
-            <span className="char-birth">{info.birth_year}</span>
-            <span className="char-gender">{info.birth_year}</span>
-            <span className="char-birth">{info.gender}</span>
-            <span className="char-species"></span>
-            <div className="char-starships">
-                <h2>Starships</h2>
+class Char extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            starshipsActive: [],
+            onClick: false,
+            starshipsHTML: ''
+        }
+    }
+
+    seeMore = (starships) => {
+        return () => {
+            this.setState({ starshipsActive: [], onClick: true });
+            const accumulator = [];
+            let starshipsHTML = ''
+            console.log('starships:::', starships);
+            if (Array.isArray(starships) && starships.length) {
+                starships.map(async (item, i) => {
+                    await axios.get(item)
+                        .then(data => {
+                            accumulator.push(data.data);
+                            this.setState({
+                                starshipsActive: accumulator,
+                            });
+
+                            starshipsHTML += "<li>" + data.data.name + "</li>";
+
+                            if ((starships.length - 1) === i) {
+                                Swal.fire({
+                                    title: 'Error!',
+                                    html: starshipsHTML,
+                                    icon: 'error',
+                                    confirmButtonText: 'Cool'
+                                })
+                            }
+                        })
+                        .catch(error => {
+                            console.log(error);
+                        }).finally(() => {
+                            console.log("Finally> ", starshipsHTML);
+
+                        });
+                });
+            }
+        }
+    };
+
+    render() {
+        return (
+            <div className="char">
+                <img src={user} />
+                <div>
+                    <h2 className="char-name">{this.props.info.name}</h2>
+                    <span className="char-birth">{this.props.info.birth_year}</span>
+                    <span className="char-gender">{this.props.info.birth_year}</span>
+                    <span className="char-birth">{this.props.info.gender}</span>
+                    <span className="char-species"></span>
+                    <div className="char-starships">
+                        {this.props.info.starships.length > 0 &&
+                            <button className="btn" onClick={this.seeMore(this.props.info.starships)}>Starships</button>
+                        }
+                    </div>
+                </div>
             </div>
-            <ul>
-                <li>Starship 1</li>
-                <li>Starship 2</li>
-            </ul>
-        </div>
-    </div>
-)
+        )
+    }
+
+}
 
 export default Char;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,153 +1,107 @@
 //Libs
-import React, { Component } from "react"
+import React, {Component} from "react"
 import axios from "axios"
-
-//Components
-
 import Pagination from "../components/Pagination"
 import Char from "../components/Char"
 import Loader from "../components/Loader"
-
 //Images
 import logo from "../images/Starwars.png"
 
-
 class IndexPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      info: [],
-      nextPage: null,
-      prevPage: null,
-      isFetching: false
+
+    setLoader = (action) => this.setState({isFetching: action});
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            info: [],
+            nextPage: null,
+            prevPage: null,
+            isFetching: false
+        }
     }
-  }
 
-  componentDidMount() {
-    axios.get("https://swapi.co/api/people/")
-      .then(data => {
-        this.setState({
-          isFetching: true
-        })
-        let _data = data.data.results;
-
-        this.setState({
-          info: _data,
-          nextPage: data.data.next,
-          prevPage: data.data.previous
-        })
-
-        /*this.state.info.map( e => {
-          ili
-        })
-
-        this.state.info.map( (e , i) => {
-
-          let starships = e.starships;
-          //console.log("STARSHIPS > ", starships);
-          if(starships.length > 0){
-            starships.map( j => {
-              axios.get(j)
-                .then( k => {
-                  //console.log("StarShip GET >>>", k.data.name);
-                  //console.log(this.state.info);
-                })
+    componentDidMount() {
+        this.setLoader(true);
+        axios.get("https://swapi.co/api/people/")
+            .then(data => {
+                let _data = data.data.results;
+                this.setState({
+                    info: _data,
+                    nextPage: data.data.next,
+                    prevPage: data.data.previous
+                });
+                this.setLoader(false);
             })
-          }
-        })*/
-      })
-      .catch(error => {
-        console.log(error);
-      })
-
-      this.setState({
-        isFetching: false
-      })
-  }
-
-  handlePagination = (url) => {
-    //Colocando return, evita da função ser invocada na view sem o evento de click
-    return (e) => {
-      console.log("handlePagination");
-      axios.get(url)
-        .then(data => {
-          this.setState({
-            isFetching: true
-          })
-
-          let _data = data.data.results
-          let _next = data.data.next
-          let _prev = data.data.previous
-
-          console.log("DATA >>>", data.data);
-          console.log("NEXT >>>", _next);
-          console.log("PREV >>>", _prev);
-
-          this.setState({
-            info: _data,
-            nextPage: _next,
-            prevPage: _prev,
-          })
-
-        })
-        .catch(error => {
-          console.error(error);
-        })
-
-        this.setState({
-          isFetching: false
-        })
+            .catch(error => {
+                console.log(error);
+            });
     }
-  }
 
-  render() {
+    handlePagination = (url) => {
+        //Colocando return, evita da função ser invocada na view sem o evento de click
+        return (e) => {
+            this.setLoader(true);
+            axios.get(url)
+                .then(data => {
 
-    const nextPage = this.state.nextPage
-    const prevPage = this.state.prevPage
-    const isFetching = this.state.isFetching
+                    let _data = data.data.results;
+                    let _next = data.data.next;
+                    let _prev = data.data.previous;
 
-    return (
-      <>
+                    this.setState({
+                        info: _data,
+                        nextPage: _next,
+                        prevPage: _prev,
+                    })
 
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+                .finally(() => {
+                    this.setLoader(false);
+                })
+        }
+    };
 
-        <div className="container">
-          <img className="logo" src={logo} />
-          <h2 className="subtitle">Characters</h2>
-          {!isFetching && <Loader />}
-          {isFetching &&
-            <ul>
-              {
-                this.state.info.map(i => (
-                  <li key={i.name}>
-                    <Char info={i} />
-                  </li>
-                ))
-              }
+    render() {
 
-            </ul>
-          }
+        const nextPage = this.state.nextPage;
+        const prevPage = this.state.prevPage;
+        const isFetching = this.state.isFetching;
 
-          <Pagination
-            handleNextPage={this.handlePagination(nextPage)}
-            handlePrevPage={this.handlePagination(prevPage)}
-            btnPrevState={prevPage}
-            btnNextState={nextPage}
-            currentPage={this.state.currentPage}
-          />
-          {/*<div className="pagination">
-            <button onClick={this.handlePrevPage} className="btn">&lt; Previous Page</button>
-            <button onClick={this.handleNextPage} className="btn">Next Page ></button>
-          </div>*/}
-
-          <footer>
-            Developed by <a href="http://rqueiroz.netlify.com/" target="_blank"> me </a> :)
-          </footer>
-
-
-        </div>
-      </>
-    )
-  }
+        return (
+            <>
+                <div className="container">
+                    <img className="logo" src={logo}/>
+                    <h2 className="subtitle">Characters</h2>
+                    {isFetching && <Loader/>}
+                    {!isFetching &&
+                    <ul>
+                        {
+                            this.state.info.map(i => (
+                                <li key={i.name}>
+                                    <Char info={i}/>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                    }
+                    <Pagination
+                        handleNextPage={this.handlePagination(nextPage)}
+                        handlePrevPage={this.handlePagination(prevPage)}
+                        btnPrevState={prevPage}
+                        btnNextState={nextPage}
+                        currentPage={this.state.currentPage}
+                    />
+                    <footer>
+                        Developed by <a href="http://rqueiroz.netlify.com/" target="_blank"> me </a> :)
+                    </footer>
+                </div>
+            </>
+        )
+    }
 }
 
 export default IndexPage
